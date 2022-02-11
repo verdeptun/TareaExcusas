b89d049daf98428bf06dc3c5d25fb197a7f823ab
verdeptun <verdeptun@gmail.com>
2022-02-11 00:35:56 +0000

    termine la tarea

--- src/app.js
+++ src/app.js
@@ -2,10 +2,32 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let aleatorio = Math.floor(Math.random() * 4);
+  let actionAleatorio = Math.floor(Math.random() * 4);
+  let whatAleatorio = Math.floor(Math.random() * 3);
+  let whenAleatorio = Math.floor(Math.random() * 5);
+
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  let excusa =
+    who[aleatorio] +
+    " " +
+    action[actionAleatorio] +
+    " " +
+    what[whatAleatorio] +
+    " " +
+    when[whenAleatorio];
+  console.log(excusa);
+
+  document.getElementById("excuse").innerHTML = excusa;
 };
